24ccaaf0c3e260534dc0993db2833bd11bc88072
Amanda <Amanda@10.0.0.103>
2024-05-14 12:30:32 -0300

    criando relacionamentos na classe Saldo

--- src/main/java/br/com/amanda/bancario/entidades/Saldo.java
+++ src/main/java/br/com/amanda/bancario/entidades/Saldo.java
@@ -1,9 +1,12 @@
 package br.com.amanda.bancario.entidades;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,5 +25,13 @@ public class Saldo {
 	@Getter
 	@Setter
 	private Double limite;
+	
+	@OneToOne
+	@Getter
+	private Cliente cliente;
+	
+	@OneToOne
+	@Getter
+	private List<Transacao> transacoes;
 
 }
