1232eedd89009dfdf4a3361b0672e2fafedf92bd
Amanda <Amanda@10.0.0.103>
2024-05-14 12:31:24 -0300

    criando relacionamentos na classe Cliente

--- src/main/java/br/com/amanda/bancario/entidades/Cliente.java
+++ src/main/java/br/com/amanda/bancario/entidades/Cliente.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,6 +29,8 @@ public class Cliente {
 	@Setter
 	private String email;
 	
-	
+	@OneToOne
+	@Getter
+	private Saldo saldo;
 
 }
